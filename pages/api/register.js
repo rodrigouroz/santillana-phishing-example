2e1d4ce7c6b278cf13f26758e9773df2ab122a0f
Rodrigo Uroz <ruroz@medallia.com>
2021-04-26 16:28:26 -0300

    Fixes issue with auth

--- pages/api/register.js
+++ pages/api/register.js
@@ -5,16 +5,16 @@ import moment from "moment";
 async function recordEntry(date, username) {
   try {
     const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
-    const jwt = new google.auth.JWT(
-      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      null,
-      // we need to replace the escaped newline characters
-      // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
-      process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      scopes
-    );
 
-    const sheets = google.sheets({ version: "v4", auth: jwt });
+    const googleAuth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY
+      },
+      scopes
+    });
+
+    const sheets = google.sheets({ version: "v4", auth: googleAuth });
 
     let values = [
       [
